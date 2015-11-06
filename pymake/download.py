b61ef278b8e5dc37c972e5e1c1c58fb408aaf0cb
Joseph D Hughes <jdhughes@usgs.gov>
2015-11-06 18:01:35 -0500

    travis

--- pymake/download.py
+++ pymake/download.py
@@ -7,7 +7,7 @@ try:
     from urllib.request import urlretrieve
 except ImportError:
     # Fall back to Python 2's urllib
-    import urllib.urlretrieve as urlretrieve
+    import urllib import urlretrieve
 
 def download_and_unzip(url, pth='./'):
     print('Attempting to download the file: ', url)
