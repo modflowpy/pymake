a020337e5e3982fec5eb837388ec0d4cf0f32b8d
Joseph D Hughes <jdhughes@usgs.gov>
2015-11-20 17:32:48 -0500

    Arrrrgggghhh! case sensitivity strikes again.

--- autotest/config004.py
+++ autotest/config004.py
@@ -8,7 +8,7 @@ exclude = ('MNW2-Fig28', 'swi2ex4sww', 'testsfr2_tab')
 
 # Release version information
 url_release = 'http://water.usgs.gov/ogw/modflow/MODFLOW-2005_v1.11.00/mf2005v1_11_00_unix.zip'
-dir_release = os.path.join(testdir, 'UNIX')
+dir_release = os.path.join(testdir, 'Unix')
 srcdir_release = os.path.join(dir_release, 'src')
 version_release = '1.11.00'
 program = 'mf2005'
