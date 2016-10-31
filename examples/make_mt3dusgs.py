8ecdc6a55d6184772e76b3d50839158302608027
Christian D Langevin <langevin@usgs.gov>
2016-10-31 08:58:46 -0400

    Added example script to build mt3dusgs

---
+++ examples/make_mt3dusgs.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+import os
+import shutil
+import pymake
+from pymake.download import download_and_unzip
+
+def make_mt3dusgs():
+
+    # get current directory
+    dstpth = os.path.join('temp')
+    if not os.path.exists(dstpth):
+        os.makedirs(dstpth)
+    os.chdir(dstpth)
+
+    # Remove the existing MT3D-USGS directory if it exists
+    dirname = 'mt3d-usgs_Distribution'
+    if os.path.isdir(dirname):
+        shutil.rmtree(dirname)
+
+    # Download the MT3D-USGS distribution
+    url = "http://water.usgs.gov/ogw/mt3d-usgs/mt3d-usgs_1.0.zip"
+    download_and_unzip(url)
+
+    # Set srcdir and target
+    srcdir = os.path.join(dirname, 'src')
+    target = 'mt3dusgs'
+
+    # Replace openspec with standard fortran
+    l = '''
+          CHARACTER*20 ACCESS,FORM,ACTION(2)
+          DATA ACCESS/'STREAM'/
+          DATA FORM/'UNFORMATTED'/
+          DATA (ACTION(I),I=1,2)/'READ','READWRITE'/
+    '''
+    fn = os.path.join(srcdir, 'openspec.inc')
+    f = open(fn, 'w')
+    f.write(l)
+    f.close()
+
+    pymake.main(srcdir, target, 'gfortran', 'gcc', makeclean=True,
+                expedite=False, dryrun=False, double=False, debug=False)
+
+    assert os.path.isfile(target), 'Target does not exist.'
+
+    # Remove the existing MT3D-USGS directory if it exists
+    if os.path.isdir(dirname):
+        shutil.rmtree(dirname)
+
+if __name__ == "__main__":
+    make_mt3dusgs()
