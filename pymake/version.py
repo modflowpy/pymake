1d25ea23bbf5da402ecd34b87bfdcc5d6bfe7d39
Joseph D Hughes <jdhughes@usgs.gov>
2017-07-12 11:12:11 -0400

    More work on pre-commit

--- pymake/version.py
+++ pymake/version.py
@@ -1,10 +1,10 @@
 #pymake version file automatically created using...pre-commit.py
-#            created on......July 12, 2017 11:01:01
+#            created on......July 12, 2017 11:11:24
 
 major = 1
 minor = 1
 micro = 0
-commit = 185
+commit = 187
 
 __version__='1.1.1'
-__build__='1.1.0.185'
+__build__='1.1.0.187'
