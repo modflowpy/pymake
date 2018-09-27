3f72caf4412af912122c1dec3282f3160c54c059
Joseph D Hughes <jdhughes@usgs.gov>
2018-09-27 10:07:55 -0400

    examples(modpath7): Update MODPATH 7 path case
    Update case for path to latest MODPATH 7 release.

--- pymake/version.py
+++ pymake/version.py
@@ -1,10 +1,10 @@
 # pymake version file automatically created using...pre-commit.py
-# created on...September 24, 2018 14:20:44
+# created on...September 27, 2018 10:07:55
 
 major = 1
 minor = 1
-micro = 49
-commit = 234
+micro = 50
+commit = 235
 
 __version__ = '{:d}.{:d}'.format(major, minor)
 __build__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
