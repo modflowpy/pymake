f96b7eaea38a3a451d01d228535a99f9744f4dff
Christian Langevin <langevin@usgs.gov>
2019-03-21 17:06:32 -0400

    update buildall.bat to build a few double precision versions for our executables repo

--- examples/buildall.bat
+++ examples/buildall.bat
@@ -1,3 +1,15 @@
+rem 64-bit executables
+python make_mf2005.py --appdir win64 --ifort --icl --double
+python make_mfnwt.py --appdir win64 --ifort --icl --double
+python make_mfusg.py --appdir win64 --ifort --icl --double
+python make_mflgr.py --appdir win64 --ifort --icl --double
 python buildall.py --appdir win64 --ifort --icl
+
+rem 32-bit executables
+python make_mf2005.py --appdir win32 --ifort --icl --double
+python make_mfnwt.py --appdir win32 --ifort --icl --double
+python make_mfusg.py --appdir win32 --ifort --icl --double
+python make_mflgr.py --appdir win32 --ifort --icl --double
 python buildall.py --appdir win32 --ifort --icl --ia32
+
 pause
