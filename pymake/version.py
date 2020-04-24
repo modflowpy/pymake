f991a740c17bd2ecb49dbbea5d3a2abfeb6a6265
Joseph D Hughes <jdhughes@usgs.gov>
2020-04-24 13:41:02 -0400

    ci(travis): update travis configuration based on flopy

--- pymake/version.py
+++ pymake/version.py
@@ -1,10 +1,10 @@
 # pymake version file automatically created using...pre-commit.py
-# created on...April 24, 2020 12:58:01
+# created on...April 24, 2020 13:41:02
 
 major = 1
 minor = 1
-micro = 141
-commit = 326
+micro = 142
+commit = 327
 
 __version__ = '{:d}.{:d}'.format(major, minor)
 __build__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
--- travis/install.sh
+++ travis/install.sh
@@ -6,6 +6,8 @@ pip install --upgrade pip
 pip install -r requirements.travis.txt
 pip install --no-binary rasterio rasterio
 pip install --upgrade numpy
+if [ "${RUN_TYPE}" = "flake" ]; then
+  pip install flake8 pylint pylint-exit
 pip install shapely[vectorize]
 pip install https://github.com/modflowpy/flopy/zipball/develop
 pip install coveralls nose-timer
