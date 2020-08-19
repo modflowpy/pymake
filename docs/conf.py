dbc24276982d0bb9c1a8df418f4b7dc3aa7df95f
jdhughes-usgs <jdhughes@usgs.gov>
2020-08-19 15:32:02 -0400

    ci(rtd): update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 
 # sys.path.insert(0, os.path.abspath('.'))
 
-from pymake import __version__
+# from pymake import __version__
 
 # -- Project information -----------------------------------------------------
 
@@ -24,8 +24,8 @@ copyright = u"2020, Christian D. Langevin"
 author = u"Christian D. Langevin"
 
 # The version.
-version = __version__
-release = __version__
+version = 1.2 # __version__
+release = 1.2 # __version__
 language = None
 
 
