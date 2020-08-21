a88f585e5de07fd437f10bbf8bf3477d94254c9f
Joseph D Hughes <jdhughes@usgs.gov>
2020-08-21 12:48:44 -0400

    refactor(rtd): reorganize source files to improve RTD API section

--- pymake/__init__.py
+++ pymake/__init__.py
@@ -47,3 +47,39 @@ from .utils.download import (
 
 # plot
 from .plot.dependency_graphs import make_plots, to_pydot
+
+# define public interfaces
+__all__ = [
+    "Pymake",
+    "__version__",
+    "main",
+    "parser",
+    "build_apps",
+    "usgs_program_data",
+    "download_and_unzip",
+    "getmfexes",
+    "repo_latest_version",
+    "get_repo_assets",
+    "zip_all",
+    "setup",
+    "setup_comparison",
+    "teardown",
+    "get_namefiles",
+    "get_entries_from_namefile",
+    "get_sim_name",
+    "get_input_files",
+    "compare_budget",
+    "compare_swrbudget",
+    "compare_heads",
+    "compare_concs",
+    "compare_stages",
+    "compare",
+    "setup_mf6",
+    "setup_mf6_comparison",
+    "get_mf6_comparison",
+    "get_mf6_files",
+    "get_mf6_blockdata",
+    "get_mf6_ftypes",
+    "get_mf6_mshape",
+    "get_mf6_nper",
+]
