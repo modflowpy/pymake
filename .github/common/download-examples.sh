cc3ff9fc96ee2d01713e3644809e153f3a9609f8
Joseph D Hughes <jdhughes@usgs.gov>
2021-03-01 08:47:53 -0600

    feat(targets): add compiler option for mt3dms, seawat, gsflow w/ gfortran-10
    closes #81

---
+++ .github/common/download-examples.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd autotest
+python ci_setup.py
+cd ..
---
+++ .github/common/install-python.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python -m pip install --upgrade pip
+pip install .
+pip install pytest pytest-cov coverage
+pip install pydotplus appdirs
+pip install matplotlib
+pip install https://github.com/modflowpy/flopy/zipball/develop
