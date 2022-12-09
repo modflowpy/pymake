46c4312c61c59cbff85b64f56119734e416d38d0
Joseph D Hughes <jdhughes@usgs.gov>
2022-12-09 11:44:11 -0600

    fix(cli): fix make-program to take ':' target value
    Fix parsing command-line arguments when using make-program.
    Add test for ':' make-program target value. Mark flaky test in
    requests autotest.

--- .github/common/install-python.sh
+++ .github/common/install-python.sh
@@ -2,7 +2,7 @@
 
 python -m pip install --upgrade pip
 pip install wheel
-pip install pytest pytest-cov pytest-xdist pytest-dependency coverage
+pip install pytest pytest-cov pytest-xdist pytest-dependency flaky coverage
 pip install appdirs matplotlib
 pip install https://github.com/modflowpy/flopy/zipball/develop
 pip install .
