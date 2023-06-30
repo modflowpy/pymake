b01eee37a1a3e786c0ca3bf1274ed7c266e04b2a
Joseph D Hughes <jdhughes@usgs.gov>
2023-06-30 15:28:44 -0500

    release(1.2.8): update mf6 version
    Also added latest version of gsflow (v 2.3.0) and mfusg_gsi (v2.1.1).

--- .github/common/install-python.sh
+++ .github/common/install-python.sh
@@ -2,7 +2,7 @@
 
 python -m pip install --upgrade pip
 pip install wheel
-pip install pytest pytest-cov pytest-xdist pytest-dependency flaky coverage
+pip install pytest pytest-cov pytest-xdist pytest-dependency pytest-benchmark pytest-cases pytest-dotenv pytest-virtualenv flaky coverage
 pip install appdirs matplotlib
 pip install https://github.com/modflowpy/flopy/zipball/develop
 pip install https://github.com/MODFLOW-USGS/modflow-devtools/zipball/develop
