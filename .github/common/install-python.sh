782e458d4c5c98f65fd312380368e608b739ff1c
Joseph D Hughes <jdhughes@usgs.gov>
2023-07-07 11:51:35 -0500

    * install test requirements for CI from pyproject.toml

--- .github/common/install-python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python -m pip install --upgrade pip
-pip install wheel
-pip install pytest pytest-cov pytest-xdist pytest-dependency flaky coverage
-pip install appdirs matplotlib
-pip install https://github.com/modflowpy/flopy/zipball/develop
-pip install .
-
