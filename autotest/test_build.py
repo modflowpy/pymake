c48f95c560d1d3e4bf6046c01bc629ca5e6850df
Joseph D Hughes <jdhughes@usgs.gov>
2024-01-08 09:07:40 -0600

    exclude sutra from meson build on windows

--- autotest/test_build.py
+++ autotest/test_build.py
@@ -19,7 +19,7 @@ targets_make = [
 test_ostag = get_ostag()
 test_fc_env = os.environ.get("FC")
 if "win" in test_ostag:
-    meson_exclude = ("mt3dms", "vs2dt", "triangle", "gridgen")
+    meson_exclude = ("mt3dms", "vs2dt", "triangle", "gridgen", "sutra")
 elif "win" not in test_ostag and test_fc_env in ("ifort",):
     meson_exclude = ("mf2000", "mf2005", "swtv4", "mflgr")
 else:
