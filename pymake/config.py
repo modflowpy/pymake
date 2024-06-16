d7a5c49ad0c9d5d4e7d418b37361eae8a52bc220
Joseph D Hughes <jdhughes@usgs.gov>
2024-06-16 17:58:22 -0500

    Release 1.2.10

--- pymake/config.py
+++ pymake/config.py
@@ -1,6 +1,6 @@
 __author__ = "Joseph D. Hughes"
-__date__ = "February 12, 2024"
-__version__ = "1.2.10.dev0"
+__date__ = "June 16, 2024"
+__version__ = "1.2.10"
 __maintainer__ = "Joseph D. Hughes"
 __email__ = "jdhughes@usgs.gov"
 __status__ = "Production"
