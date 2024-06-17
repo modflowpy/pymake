006b6ba9cbab5073686e19ff2dc00dafbf923ada
jdhughes-usgs <jdhughes@usgs.gov>
2024-06-17 10:49:21 -0500

    ci: update code.json for release workflow (#187)

--- pymake/config.py
+++ pymake/config.py
@@ -1,9 +1,6 @@
-# mfpymake version file automatically created using
-# update_version.py on June 16, 2024 18:36:44
-
 __author__ = "Joseph D. Hughes"
-__date__ = "June 16, 2024"
-__version__ = "1.2.10"
+__date__ = "June 17, 2024"
+__version__ = "1.2.10.dev0"
 __maintainer__ = "Joseph D. Hughes"
 __email__ = "jdhughes@usgs.gov"
 __status__ = "Production"
--- scripts/update_version.py
+++ scripts/update_version.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import re
 import textwrap
 from datetime import datetime
@@ -14,11 +15,13 @@ _version_py_path = _project_root_path / "pymake" / "config.py"
 
 # file names and the path to the file relative to the repo root directory
 file_paths_list = [
+    _project_root_path / "code.json",
     _project_root_path / "README.md",
     _project_root_path / "version.txt",
     _project_root_path / "pymake" / "config.py",
 ]
 file_paths = {pth.name: pth for pth in file_paths_list}  # keys for each file
+
 
 def split_nonnumeric(s):
     match = re.compile("[^0-9]").search(s)
@@ -29,20 +32,32 @@ _initial_version = Version("0.0.1")
 _current_version = Version(_version_txt_path.read_text().strip())
 
 
-def update_version_txt(version: Version):
-    with open(_version_txt_path, "w") as f:
+def update_version_txt(version: Version) -> None:
+    """Update version number in version.txt
+
+    Parameters
+    ----------
+    version : Version
+        version number
+    """
+    with open(_version_txt_path, "w", encoding="utf8") as f:
         f.write(str(version))
     print(f"Updated {_version_txt_path} to version {version}")
 
 
-def update_version_py(timestamp: datetime, version: Version):
+def update_version_py(timestamp: datetime, version: Version) -> None:
+    """Update version number in config.py
+
+    Parameters
+    ----------
+    timestamp : datetime
+        current datetime
+    version : Version
+        version number
+    """
     lines = file_paths["config.py"].read_text().rstrip().split("\n")
-    
-    with open(_version_py_path, "w") as f:
-        f.write(
-            f"# {_project_name} version file automatically created using\n"
-            f"# {Path(__file__).name} on {timestamp:%B %d, %Y %H:%M:%S}\n\n"
-        )
+
+    with open(_version_py_path, "w", encoding="utf8") as f:
         for line in lines:
             if "__date__" in line:
                 line = f'__date__ = "{timestamp:%B %d, %Y}"'
@@ -52,18 +67,22 @@ def update_version_py(timestamp: datetime, version: Version):
     print(f"Updated {_version_py_path} to version {version}")
 
 
+def update_readme_markdown(version: Version) -> None:
+    """Update README.md
 
-def update_readme_markdown(
-    timestamp: datetime, version: Version, approved: bool = False
-):
+    Parameters
+    ----------
+    version : Version
+        version number
+    """
     fpth = file_paths["README.md"]
-    
+
     # read README.md into memory
     lines = fpth.read_text().rstrip().split("\n")
 
     # rewrite README.md
     terminate = False
-    with open(fpth, "w") as f:
+    with open(fpth, "w", encoding="utf8") as f:
         for line in lines:
             if "### Version " in line:
                 line = f"### Version {version}"
@@ -73,11 +92,43 @@ def update_readme_markdown(
 
     print(f"Updated {fpth} to version {version}")
 
+
+def update_codejson(version: Version) -> None:
+    """Update code.json
+
+    Parameters
+    ----------
+    version : Version
+        version number
+    """
+    # define json filename
+    json_fname = file_paths["code.json"]
+
+    # load and modify json file
+    data = json.loads(json_fname.read_text())
+
+    # rewrite the json file
+    with open(json_fname, "w", encoding="utf8") as f:
+        json.dump(data, f, indent=4)
+        f.write("\n")
+
+    print(f"Updated {json_fname} to version {version}")
+
+
 def update_version(
     timestamp: datetime = datetime.now(),
     version: Version = None,
-    approved: bool = False,
-):
+) -> None:
+    """Main function for updating all of the files containing
+       version information
+
+    Parameters
+    ----------
+    timestamp : datetime, optional
+        datetime object, by default datetime.now()
+    version : Version, optional
+        version number, by default None
+    """
     lock_path = Path(_version_txt_path.name + ".lock")
     try:
         lock = FileLock(lock_path)
@@ -91,7 +142,8 @@ def update_version(
         with lock:
             update_version_txt(version)
             update_version_py(timestamp, version)
-            update_readme_markdown(timestamp, version, approved)
+            update_readme_markdown(version)
+            update_codejson(version)
     finally:
         try:
             lock_path.unlink()
@@ -105,11 +157,12 @@ if __name__ == "__main__":
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent(
             """\
-            Update version information stored in version.txt in the project root,
-            as well as several other files in the repository. If --version is not
-            provided, the version number will not be changed. A file lock is held
-            to synchronize file access. The version tag must comply with standard
-            '<major>.<minor>.<patch>' format conventions for semantic versioning.
+            Update version information stored in version.txt in the project
+            root, as well as several other files in the repository. If 
+            --version is not provided, the version number will not be 
+            changed. A file lock is held to synchronize file access. The 
+            version tag must comply with standard '<major>.<minor>.<patch>' 
+            format conventions for semantic versioning.
             """
         ),
     )
@@ -120,18 +173,12 @@ if __name__ == "__main__":
         help="Specify the release version",
     )
     parser.add_argument(
-        "-a",
-        "--approve",
-        required=False,
-        action="store_true",
-        help="Approve the release (defaults false)",
-    )
-    parser.add_argument(
         "-g",
         "--get",
         required=False,
         action="store_true",
-        help="Just get the current version number, no updates (defaults false)",
+        help="Just get the current version number, "
+        + "no updates (defaults false)",
     )
     args = parser.parse_args()
 
@@ -145,5 +192,4 @@ if __name__ == "__main__":
             version=(
                 Version(args.version) if args.version else _current_version
             ),
-            approved=args.approve,
         )
