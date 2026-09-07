ee204a7911b597c6d3752151b0a8ae16199b54b3
Joseph Hughes <jdhughes99+1@gmail.com>
2026-09-07 10:24:47 -0500

    refactor: plot the dependency graphs with an optional pydot (#351)
    pydotplus is a fork of pydot that has not been released since 2014, and was imported when pymake was imported. pydot provides the same interface, is maintained, and is what the networkx dot interface is written against.
    
    pydot is now an optional dependency, installed with 'pip install mfpymake[plot]'. Plotting a dependency graph also needs the graphviz binaries, which pip cannot install, so an install no longer carries a dependency it may not be able to use. make_plots and to_pydot report what to install when pydot is missing.

--- pymake/plot/__init__.py
+++ pymake/plot/__init__.py
@@ -1,3 +1,4 @@
 """Functions to plot source code dependencies determined using a directed
-acyclic graph (DAG). pydotplus is used to plot the DAG.
+acyclic graph (DAG). pydot, which is an optional dependency, is used to
+plot the DAG.
 """
